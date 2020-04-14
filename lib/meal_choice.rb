5809c252f21888ce0e977e81a8143360e7b473a4
Logan McGuire <lrmcguire93@gmail.com>
2020-04-14 21:30:12 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,7 @@
-# Your code here
-# For output purposes, use "puts" instead of "print" or "p"
+def meal_choice (veggie1, veggie2, protein = "tofu")
+  puts "What a nutritious meal!";
+  puts "A plate of #{protein} with #{veggie1} and #{veggie2}."
+  "A plate of #{protein} with #{veggie1} and #{veggie2}."
+  #Cleaner if you assign a variable to the second puts. 
+  #Then puts the variable and end with the variable instead of sentence *2.
+end
